c06d1c65f0a0ef2f7c4525ced689ed203ced65dd
maribvidal <marianobazalarvidal@gmail.com>
2024-07-03 15:51:27 -0300

    Agregué un sistema de coloreo y de cambio de tamaño

--- script.js
+++ script.js
@@ -2,24 +2,79 @@
 
 //  - Global Variables
 
+const HEX_RED = "#FF0000";
+const HEX_BLUE = "#0000ff";
+const HEX_GREEN = "#00ff00";
+const HEX_WHITE = "#ffffff";
+
 const mainContainer = document.createElement("div");
 mainContainer.id = "main-container";
 
-const GRID_SIZE = 16;
+const optionsContainer = document.createElement("div");
+optionsContainer.id = "options-container";
 
-let gridArray;
+const optionRed = document.createElement("option");
+optionRed.value = HEX_RED;
+optionRed.textContent = "Red";
+
+const optionBlue = document.createElement("option");
+optionBlue.value = HEX_BLUE;
+optionBlue.textContent = "Blue";
+
+const optionGreen = document.createElement("option");
+optionGreen.value = HEX_GREEN;
+optionGreen.textContent = "Green";
+
+const optionWhite = document.createElement("option");
+optionWhite.value = HEX_WHITE;
+optionWhite.textContent = "White";
+
+const selectColor = document.createElement("select");
+selectColor.name = "selectColor";
+
+const btnChangeGridSize = document.createElement("button");
+btnChangeGridSize.textContent = "Change Grid Size";
+
+const btnToggleGrid = document.createElement("button");
+btnToggleGrid.textContent = "Toggle Grid";
+
+let gridArray = [];
+let gridSize = 16;
+let currentColor = HEX_RED;
 
 //  - Functions
+
+function changeCurrentColor() {
+    currentColor = selectColor.value;
+}
+
+function changeGridSize() {
+    let number = +prompt("Choose a number between 1 and 100", 16);
+    if (number < 1 || number > 100)
+        return false;
+
+    gridSize = number;
+    return true;
+}
+
+function calcFlexBasis() {
+    return (100 / gridSize).toString() + "%";
+}
 
 function createGrid() {
     let array = [];
     // ROWS
-    for (let i = 0; i < GRID_SIZE; i++) {
+    for (let i = 0; i < gridSize; i++) {
         array[i] = [];
         // COLUMNS
-        for (let j = 0; j < GRID_SIZE; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const newDiv = document.createElement("div");
             newDiv.className = "square";
+            newDiv.style.flexBasis = calcFlexBasis();
+            newDiv.addEventListener("click", (event) => {
+                let target = event.target;
+                target.style.backgroundColor = currentColor;
+            });
             array[i][j] = newDiv;
         }
     }
@@ -28,15 +83,47 @@ function createGrid() {
 
 // Appends the gridArray elements to the main div
 function appendGrid(array) {
-    for (let i = 0; i < GRID_SIZE; i++) {
-        for (let j = 0; j < GRID_SIZE; j++) {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
             mainContainer.appendChild(array[i][j]);
+        }
+    }
+}
+
+function removeGrid(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            array[i][j].remove();
         }
     }
 }
 
 //  - General logic
 
+selectColor.appendChild(optionRed);
+selectColor.appendChild(optionBlue);
+selectColor.appendChild(optionGreen);
+selectColor.appendChild(optionWhite);
+optionsContainer.appendChild(selectColor);
+optionsContainer.appendChild(btnChangeGridSize);
+
 gridArray = createGrid(gridArray);
 appendGrid(gridArray);
+
 document.body.appendChild(mainContainer);
+document.body.appendChild(optionsContainer);
+
+// Change the currentColor with the select button
+selectColor.addEventListener("change", changeCurrentColor);
+
+// Change the grid size
+btnChangeGridSize.addEventListener("click", () => {
+    if (!changeGridSize()) {
+        alert("ERROR: Invalid input");
+    } else {
+        removeGrid(gridArray);
+        gridArray = [];
+        gridArray = createGrid();
+        appendGrid(gridArray);
+    }
+});
